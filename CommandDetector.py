0d4f55c8c4a967eb2dceb881acc5fd4c2ffdf628
evertonscoelho <evertonscoelho@gmail.com>
2018-08-13 21:30:02 -0300

    checkpoint novo

--- CommandDetector.py
+++ CommandDetector.py
@@ -17,9 +17,12 @@ for linha in textInput:
 	nameFile = split[0]
 	resultExpected = split[1]
 	resultExpected = resultExpected.replace("\n", "")
-
-
 	image = cv2.imread(pathTeste + nameFile)
+
+	r = 1100.0 / image.shape[1]
+	dim = (1100, int(image.shape[0] * r))
+	image = cv2.resize(image,dim, interpolation = cv2.INTER_AREA)
+
 	pre_proc = Command.preprocess_image(image)
 	cnts = Command.find_cnts_commands(pre_proc)
 	commands = Command.find_commands(cnts, image, train_commands)
@@ -30,21 +33,20 @@ for linha in textInput:
 		commands = Command.find_commands(cnts, image, train_commands)
 		response = Command.responseCommands(commands)
 
-	if response == resultExpected:
+	if response.lower() == resultExpected.lower():
 		textOutput = textOutput + "1;"
 	else:
 		textOutput = textOutput +"0;"
 	textOutput = textOutput + nameFile +";"
 	textOutput = textOutput + response +"\n"
-
+	
 	temp_cnts = []
 	for y in range(len(commands)):
 		for x in range(len(commands[y])):
 			temp_cnts.append(commands[y][x].contour)
 			cv2.drawContours(image,temp_cnts, -1, (255,0,0), 2)
 			cv2.putText(image,commands[y][x].best_command_match,(commands[y][x].center[0]-60, commands[y][x].center[1]+25),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,0),3,cv2.LINE_AA)
-			cv2.imwrite(path+"/testeResultado"+ nameFile +".jpeg", image); 
-    
+			cv2.imwrite(path+"/testeResultado"+ nameFile +".jpeg", image);     
 
 arqOutput = open(pathTeste+"testOutput.txt", 'w')
 arqOutput.write(textOutput)
